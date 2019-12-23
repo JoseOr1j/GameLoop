ab8f7bd9aceaf48a250d662770dee8167aa4ba7d
Jose Orozco <JoseOrozco1j@gmail.com>
2019-12-23 19:36:38 +0000

    Changed Mechanics, variable names, game prompts, added more comments.

--- main.cs
+++ main.cs
@@ -1,7 +1,10 @@
-/* MECHANICS AND RULES:
- While i is less than j then the loop will state the values of i and j. j is being incremented by one, while i is being multiplied by 2.
- The point of the game is to catch up to the value of j to 
- become an awesome player, although you can put close numbers such as (0,1), (0,2), (1,2), there is no fun in putting small numbers, go big for j and see how fast i catches up!*/
+/* Designed and created by: Jose Orozco. 
+Purpose: A Looping Game, created: 12/23/19
+RULES AND MECHANICS: A game in where the user has to input two values one smaller and the other bigger.
+The smaller value will be multiplied by 2 every turn (loop) and the bigger value will be incremented by
+1 every turn (loop). The smaller value must be initially smaller in order to properly play, examples like:
+(s -> smaller, b -> bigger) (s,b) -> (2,0) Results in an automatic win.
+(0,100) Results in a longer lasting game.*/
 
 using System;
 using static System.Console;
@@ -11,9 +14,9 @@ namespace Games
 class MainClass {
   // begin Main Program
   public static void Main (string[] args) {
-    // Asks user to play 
-    WriteLine("PLAY? Yes/No?");
-    // Reads response, no need to convert
+    // asks user to play 
+    WriteLine("Would you like to play this Looping Game? Yes/No?");
+    // reads response, no need to convert
     string playAgain = ReadLine();
     // if playAgain is a NO response then the game ends
     if(playAgain == "No" || playAgain == "no")
@@ -27,47 +30,52 @@ class MainClass {
     // gets players name
     string name = ReadLine();
     // What is said before the game begins
-    WriteLine("You are currently not awesome yet player " + name);
+    WriteLine("Welcome Player: " + name);
     WriteLine("Let us begin! Enter two values integer values, one small and one big to begin the game.");
     // declares and converts integer values into string literals
-    int BIG = Convert.ToInt32(ReadLine());
     int SMALL = Convert.ToInt32(ReadLine());
+    int BIG = Convert.ToInt32(ReadLine());
     // states your chosen values
-    WriteLine("Your selcted values are: " + BIG + " " +  "and" + " " + SMALL);
+    WriteLine("Your selcted values are: " + SMALL + " " +  "and" + " " + BIG);
+    /*WriteLine("Press any key to begin");
+    ReadKey(); // To make the game pause, removed for speed
+    WriteLine(" ");*/
+  
     WriteLine("GAME START!");
     // begin while loop 
-while (BIG < SMALL)
+while (SMALL <= BIG)
 {
-  // if i is greater than j then the game ends
-  if(BIG > SMALL)
+  // if small is greater than big then the game ends
+  if(SMALL >= BIG)
   {
     break;
   }
-  // if the i is less than j then the game continues
+  // if the small is less than big then the game continues
   else 
   {
-    WriteLine("You are not awesome YET! player: " + name);
+    WriteLine("You have not caught up to the bigger value, Player: " + name);
   }
-  // states values to make it easy to see the progress of the j and i
+  // states values to make it easy to see the progress of the small and big
 WriteLine(SMALL);
 SMALL++;
-// slower pace j incremented by 1
+SMALL = SMALL * 2;
+// faster paced small value multiplied by 2
 WriteLine(BIG);
 BIG++;
-// faster paced i multiplied by 2
-BIG = BIG * 2;
+// slower paced big value incremented by 1
 }
 // end of loop
-WriteLine("You are now awesome! player: " + name);
+WriteLine("You caught up to the bigger value! Player: " + name + "!");
 // thanks user and asks if they would like to play again
-WriteLine("Thank you for playing my game, wanna go again? Yes/No?");
+WriteLine("Thank you for playing my game, want to go again? Yes/No?");
 // reads input
 playAgain = ReadLine();
-// if input is a N or n for No then the game ends 
+// if input is a No or no then the game ends
  if(playAgain == "No" || playAgain == "no")
-    WriteLine("GAME END");
+    WriteLine("GAME END!");
 }
   }
 
   }
 }
+// To Do: Add exception handling for I/O
